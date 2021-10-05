6c9064a719437be5fce538cb99558cf298777c0a
KelechiOdom10 <kelechiodom10@gmail.com>
2021-10-05 02:12:09 +0100

    Color mode switcher

---
+++ components/post/UpVoteButton.tsx
@@ -0,0 +1,36 @@
+import { Icon, Button, VStack, useColorModeValue } from "@chakra-ui/react";
+import React from "react";
+import { BiUpArrow } from "react-icons/bi";
+import { BsFillCaretUpFill } from "react-icons/bs";
+
+export default function UpVoteButton({
+  isFavorited,
+  count,
+  ...props
+}: {
+  isFavorited: boolean;
+  count: number;
+}) {
+  return (
+    <VStack
+      as={Button}
+      variant="unstyled"
+      align="center"
+      fontSize={{
+        base: isFavorited ? "sm" : "xs",
+        md: isFavorited ? "md" : "sm",
+      }}
+      fontWeight={isFavorited && "extrabold"}
+      color={useColorModeValue("brand.900", "white")}
+      {...props}
+    >
+      <Icon
+        fontSize={{ base: isFavorited ? "xl" : "lg", md: isFavorited && "2xl" }}
+        color="inherit"
+        as={isFavorited ? BsFillCaretUpFill : BiUpArrow}
+        aria-label="Toggle Like"
+      />
+      <span>{count}</span>
+    </VStack>
+  );
+}
