328d966d3ddc6e3d17c1394cf158cb3d23fd2357
KelechiOdom10 <kelechiodom10@gmail.com>
2021-10-05 02:11:58 +0100

    Color mode switcher

---
+++ components/utils/ColorModeSwitcher.tsx
@@ -0,0 +1,29 @@
+import {
+  useColorMode,
+  useColorModeValue,
+  IconButton,
+  IconButtonProps,
+} from "@chakra-ui/react";
+import { FaMoon } from "react-icons/fa";
+import { HiSun } from "react-icons/hi";
+
+type ColorModeSwitcherProps = Omit<IconButtonProps, "aria-label">;
+
+export const ColorModeSwitcher = (props: ColorModeSwitcherProps) => {
+  const { toggleColorMode } = useColorMode();
+  const text = useColorModeValue("dark", "light");
+  const color = useColorModeValue("brand.900", "white");
+  const SwitchIcon = useColorModeValue(FaMoon, HiSun);
+
+  return (
+    <IconButton
+      size={useColorModeValue("sm", "md")}
+      variant="ghost"
+      color={color}
+      onClick={toggleColorMode}
+      icon={<SwitchIcon />}
+      aria-label={`Switch to ${text} mode`}
+      {...props}
+    />
+  );
+};
