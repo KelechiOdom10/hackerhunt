956f3b93a398edf3440c270c7e44d6d3c75b23c1
kelechiodom10 <kelechiodom10@gmail.com>
2021-10-08 20:08:29 +0100

    Updated Logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,22 +1,25 @@
-import { Text, Link } from "@chakra-ui/react";
+import { Text, TextProps } from "@chakra-ui/react";
+import CustomLink from "./utils/CustomLink";
 
-export default function Logo() {
+export default function Logo({ ...props }: TextProps) {
   return (
-    <Link href="/">
+    <CustomLink href="/">
       <Text
         fontFamily="heading"
-        fontSize={{ md: "xl" }}
+        fontSize="xl"
         display={{ base: "none", md: "block" }}
+        {...props}
       >
         Hacker Hunt
       </Text>
       <Text
         fontFamily="heading"
-        fontSize={{ base: "lg" }}
+        fontSize="lg"
         display={{ base: "block", md: "none" }}
+        {...props}
       >
         HH
       </Text>
-    </Link>
+    </CustomLink>
   );
 }
