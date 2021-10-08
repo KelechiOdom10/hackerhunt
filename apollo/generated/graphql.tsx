e1add40cd2e60fcb59ed9885c9f50e26f2e00cce
kelechiodom10 <kelechiodom10@gmail.com>
2021-10-08 20:11:28 +0100

    Generated graphql types

--- apollo/generated/graphql.tsx
+++ apollo/generated/graphql.tsx
@@ -1,13 +1,8 @@
+import { gql } from '@apollo/client';
 export type Maybe<T> = T | null;
-export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]?: Maybe<T[SubKey]>;
-};
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]: Maybe<T[SubKey]>;
-};
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -15,119 +10,129 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
+  DateTime: any;
 };
 
 export type AuthPayload = {
-  __typename?: "AuthPayload";
-  token: Scalars["String"];
+  __typename?: 'AuthPayload';
+  token: Scalars['String'];
   user: User;
 };
 
 export type Comment = {
-  __typename?: "Comment";
-  id: Scalars["ID"];
+  __typename?: 'Comment';
+  createdAt: Scalars['DateTime'];
+  id: Scalars['ID'];
   link: Link;
-  text: Scalars["String"];
+  text: Scalars['String'];
   user: User;
 };
 
 export type Link = {
-  __typename?: "Link";
+  __typename?: 'Link';
   comments?: Maybe<Array<Maybe<Comment>>>;
-  description?: Maybe<Scalars["String"]>;
-  id: Scalars["ID"];
-  image?: Maybe<Scalars["String"]>;
-  title: Scalars["String"];
-  url: Scalars["String"];
+  createdAt: Scalars['DateTime'];
+  description?: Maybe<Scalars['String']>;
+  id: Scalars['ID'];
+  image?: Maybe<Scalars['String']>;
+  title: Scalars['String'];
+  url: Scalars['String'];
   user?: Maybe<User>;
 };
 
 export type LinkInput = {
-  title: Scalars["String"];
-  url: Scalars["String"];
+  title: Scalars['String'];
+  url: Scalars['String'];
 };
 
 export type Mutation = {
-  __typename?: "Mutation";
+  __typename?: 'Mutation';
   createComment: Comment;
   createLink: Link;
   signIn: AuthPayload;
   signUp: AuthPayload;
-  toggleVote: Scalars["Boolean"];
+  toggleVote: Scalars['Boolean'];
 };
+
 
 export type MutationCreateCommentArgs = {
-  id: Scalars["ID"];
-  text: Scalars["String"];
+  id: Scalars['ID'];
+  text: Scalars['String'];
 };
+
 
 export type MutationCreateLinkArgs = {
-  description: Scalars["String"];
-  url: Scalars["String"];
+  title: Scalars['String'];
+  url: Scalars['String'];
 };
+
 
 export type MutationSignInArgs = {
-  email: Scalars["String"];
-  password: Scalars["String"];
+  email: Scalars['String'];
+  password: Scalars['String'];
 };
+
 
 export type MutationSignUpArgs = {
-  email: Scalars["String"];
-  password: Scalars["String"];
-  username: Scalars["String"];
+  email: Scalars['String'];
+  password: Scalars['String'];
+  username: Scalars['String'];
 };
 
+
 export type MutationToggleVoteArgs = {
-  id: Scalars["ID"];
+  id: Scalars['ID'];
 };
 
 export type Query = {
-  __typename?: "Query";
+  __typename?: 'Query';
   feed: Array<Link>;
   link: Link;
   me: User;
-  sayHello?: Maybe<Scalars["String"]>;
+  sayHello?: Maybe<Scalars['String']>;
   user: User;
 };
 
+
 export type QueryLinkArgs = {
-  id: Scalars["ID"];
+  id: Scalars['ID'];
 };
 
+
 export type QueryUserArgs = {
-  id: Scalars["ID"];
+  id: Scalars['ID'];
 };
 
 export type SignInInput = {
-  email: Scalars["String"];
-  password: Scalars["String"];
+  email: Scalars['String'];
+  password: Scalars['String'];
 };
 
 export type SignUpInput = {
-  email: Scalars["String"];
-  password: Scalars["String"];
-  username: Scalars["String"];
+  email: Scalars['String'];
+  password: Scalars['String'];
+  username: Scalars['String'];
 };
 
 export type User = {
-  __typename?: "User";
+  __typename?: 'User';
   comments: Array<Comment>;
-  email: Scalars["String"];
-  id: Scalars["ID"];
+  email: Scalars['String'];
+  id: Scalars['ID'];
   links: Array<Link>;
-  username: Scalars["String"];
+  username: Scalars['String'];
 };
 
 export type CreateCommentInput = {
-  id: Scalars["ID"];
-  text: Scalars["String"];
+  id: Scalars['ID'];
+  text: Scalars['String'];
 };
 
 export type CreateLinkInput = {
-  title: Scalars["String"];
-  url: Scalars["String"];
+  title: Scalars['String'];
+  url: Scalars['String'];
 };
 
 export type IdInput = {
-  id: Scalars["ID"];
+  id: Scalars['ID'];
 };
