469b39b7edd8b4c7c4b7115e66bc93d8c3f4efe1
kelechiodom10 <kelechiodom10@gmail.com>
2021-10-14 21:26:56 +0100

    Created higher order authentication components

---
+++ hoc/withNoAuth.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+import { NextPage } from "next";
+import { useEffect } from "react";
+import LoadingScreen from "../components/utils/LoadingScreen";
+import { useMe } from "../hooks/useMe";
+
+export default function withoutAuth(
+  WrappedComponent: NextPage,
+  location?: string
+) {
+  function NoAuthComponent(props) {
+    const router = useRouter();
+    const { me, loading } = useMe();
+
+    useEffect(() => {
+      if (loading || !me) return;
+      router.replace(location || "/");
+    }, [loading, me, router, location]);
+
+    if (loading || me) {
+      return <LoadingScreen />;
+    }
+
+    return <WrappedComponent {...props} />;
+  }
+
+  return NoAuthComponent;
+}
