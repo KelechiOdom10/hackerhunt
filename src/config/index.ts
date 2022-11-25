9dcf5d25770a7ce77e42b2b2f2b9bf4bd40f66b6
Kelechiodom10 <kelechiodom10@gmail.com>
2022-11-25 15:19:40 +0000

    Create config file with constants

---
+++ src/config/index.ts
@@ -0,0 +1,19 @@
+// else {
+//   const hostname = typeof window !== "undefined" && window?.location?.hostname;
+//   if (hostname) {
+//     if (hostname.includes("hostname")) {
+//       env = "production";
+//     } else {
+//       env = "development";
+//     }
+//   } else {
+//     env = "development";
+//   }
+// }
+
+export const IS_PRODUCTION = process.env.NODE_ENV === "production";
+export const IS_DEV = !IS_PRODUCTION;
+
+export const API_URL = IS_PRODUCTION
+  ? `${process.env.NEXT_PUBLIC_URL}/api/graphql`
+  : "http://localhost:3000/api/graphql";
