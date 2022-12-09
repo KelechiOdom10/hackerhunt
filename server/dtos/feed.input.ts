e14db07ab7d9dfa67c97fe0a03fa2cbb952916c3
Kelechiodom10 <kelechiodom10@gmail.com>
2022-12-09 16:57:15 +0000

    Create feed input that handles sorting, filtering and pagination of links

---
+++ server/dtos/feed.input.ts
@@ -0,0 +1,17 @@
+import { Field, InputType, Int } from "type-graphql";
+
+// Define the input arguments for filtering and pagination
+@InputType()
+export class FeedArgs {
+  @Field(() => String, { nullable: true })
+  filter?: string;
+
+  @Field(() => String, { nullable: true })
+  orderBy?: "createdAt" | "votes" = "votes";
+
+  @Field(() => Int, { nullable: true, defaultValue: 0 })
+  skip?: number = 0;
+
+  @Field(() => Int, { nullable: true, defaultValue: 10 })
+  take?: number = 10;
+}
