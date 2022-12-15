227eefd242f1727ff4ba35d102af83496b0a7958
Kelechiodom10 <kelechiodom10@gmail.com>
2022-12-15 11:04:57 +0000

    Add next progress bar to show that user is navigating through pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,22 @@
 import { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, useColorModeValue } from "@chakra-ui/react";
 import { ApolloProvider } from "@apollo/client";
 import theme from "../utils/theme";
 import { useApollo } from "../apollo/client";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState);
+  const color = useColorModeValue("#0a0a0a", "#FFFFFF");
+
   return (
     <ApolloProvider client={client}>
       <ChakraProvider resetCSS theme={theme}>
+        <NextNProgress
+          color={color}
+          height={4}
+          options={{ showSpinner: false }}
+        />
         <Component {...pageProps} />
       </ChakraProvider>
     </ApolloProvider>
