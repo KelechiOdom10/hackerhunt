d58ac8aea7ce8023f161eb69ea1bf20e6a0d2359
Kelechiodom10 <kelechiodom10@gmail.com>
2022-12-17 21:26:59 +0000

    Create user comment component and list container

---
+++ src/components/user/comment/UserCommentList.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { CommentDetailsFragment } from "~/apollo/generated/graphql";
+import UserComment from "./UserComment";
+import {
+  Heading,
+  StackDivider,
+  VStack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+type Props = {
+  comments: CommentDetailsFragment[];
+};
+
+const UserCommentList = ({ comments }: Props) => {
+  const bgColor = useColorModeValue("white", "gray.800");
+
+  return (
+    <VStack
+      spacing={3}
+      bg={bgColor}
+      w="full"
+      rounded="md"
+      p={4}
+      divider={<StackDivider />}
+      borderWidth={useColorModeValue(1, 0)}
+    >
+      {comments.length === 0 ? (
+        <Heading
+          fontSize={{ base: "xs", md: "sm", lg: "md" }}
+          fontFamily="Lato"
+          alignSelf="start"
+        >
+          No comments yet
+        </Heading>
+      ) : (
+        comments.map(comment => (
+          <UserComment comment={comment} key={comment.id} />
+        ))
+      )}
+    </VStack>
+  );
+};
+
+export default UserCommentList;
