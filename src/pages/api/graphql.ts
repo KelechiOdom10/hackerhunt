d92482a05728390fcd6e207c1a2e71f8b6b79a07
Kelechiodom10 <kelechiodom10@gmail.com>
2022-12-19 17:25:11 +0000

    ok

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,16 +1,24 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import "reflect-metadata";
-import Cors from "micro-cors";
-// import { ApolloServer } from "@apollo/server";
-// import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
-// import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import Cors from "cors";
 // import { gql } from "@apollo/client";
 import { PrismaClient, User } from "@prisma/client";
 import prisma from "server/db";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getUser } from "server/utils/auth";
-import { ApolloServer } from "apollo-server-micro";
-import { schema } from "server/schema";
+import {
+  AuthResolver,
+  CommentResolver,
+  JobResolver,
+  LinkResolver,
+  UserResolver,
+  VoteResolver,
+} from "server/resolvers";
+import { GraphQLError } from "graphql";
+import { validate } from "class-validator";
+import { buildSchema } from "type-graphql";
 // import { makeExecutableSchema } from "@graphql-tools/schema";
 
 export interface GraphQLContext {
@@ -34,78 +42,53 @@ async function createContext(req: NextApiRequest, res: NextApiResponse) {
 }
 
 // Setup cors
-// const cors = Cors({
-//   methods: ["GET", "HEAD", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-
-//   credentials: true,
-//   origin: [
-//     "https://studio.apollographql.com",
-//     "http://localhost:3000",
-//     `${process.env.NEXT_PUBLIC_VERCEL_URL}`,
-//   ],
-// });
-
-const typeSchema = await schema();
-
-const apolloServer = new ApolloServer({
-  schema: typeSchema,
-  context: createContext,
-});
-
-const startServer = apolloServer.start();
-
 const cors = Cors({
-  origin: "https://studio.apollographql.com",
-  allowCredentials: true,
-  allowMethods: ["GET", "POST", "PUT", "DELETE"],
-  allowHeaders: [
-    "access-control-allow-credentials",
-    "access-control-allow-methods: POST",
-    "access-control-allow-origin",
-    "content-type",
+  methods: ["GET", "HEAD", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  credentials: true,
+  origin: [
+    "https://studio.apollographql.com",
+    "http://localhost:8000",
+    "http://localhost:3000",
   ],
-  // origin: [
-  //   "https://studio.apollographql.com",
-  //   "http://localhost:3000",
-  //   `${process.env.NEXT_PUBLIC_VERCEL_URL}`,
-  // ],
 });
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.setHeader("access-control-allow-credentials", "true");
-  res.setHeader(
-    "access-control-allow-Origin",
-    "https://studio.apollographql.com"
-  );
-  res.setHeader(
-    "access-control-allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, access-control-allow-Methods, access-control-allow-Origin, access-control-allow-credentials, access-control-allow-Headers"
-  );
-  res.setHeader(
-    "access-control-allow-Methods",
-    "POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD"
-  );
-
-  if (req.method == "OPTIONS") {
-    res.setHeader(
-      "access-control-allow-Methods",
-      "PUT, POST, PATCH, DELETE, GET"
-    );
-    res.end();
-    return false;
-  }
-
-  await startServer;
-
-  await apolloServer.createHandler({
-    path: "/api/graphql",
-  })(req, res);
-};
-
-export default handler;
-
-export const config = {
-  api: {
-    bodyParser: false,
+const schema = await buildSchema({
+  emitSchemaFile: {
+    path: "../../apollo/schema.graphql",
   },
-};
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  resolvers: [
+    UserResolver,
+    AuthResolver,
+    CommentResolver,
+    VoteResolver,
+    LinkResolver,
+    JobResolver,
+  ],
+  validate: async argValue => {
+    const errors = await validate(argValue);
+    if (errors.length > 0) {
+      const message = Object.values(errors[0].constraints)[0];
+      throw new GraphQLError(message || "Argument Validation Error", {
+        extensions: {
+          code: "BAD_USER_INPUT",
+          validationErrors: errors,
+          message: "One or more fields are invalid",
+          http: {
+            status: 400,
+          },
+        },
+      });
+    }
+  },
+});
+
+const apolloServer = new ApolloServer<GraphQLContext>({
+  schema,
+  plugins: [ApolloServerPluginLandingPageLocalDefault()],
+  introspection: true,
+});
+
+export default startServerAndCreateNextHandler(apolloServer, {
+  context: async (req, res) => ({ req, res, prisma, user: await getUser(req) }),
+});
