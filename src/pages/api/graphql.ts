490a2c26070411d83abccabb53f522b0e3648d41
Kelechiodom10 <kelechiodom10@gmail.com>
2022-12-19 15:46:38 +0000

    try this

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,11 +1,13 @@
 import "reflect-metadata";
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { gql } from "@apollo/client";
 import { PrismaClient, User } from "@prisma/client";
 import prisma from "server/db";
 import { NextApiRequest, NextApiResponse } from "next";
-import { schema } from "server/schema";
+// import { schema } from "server/schema";
 import { getUser } from "server/utils/auth";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
 export interface GraphQLContext {
   req: NextApiRequest;
@@ -13,6 +15,30 @@ export interface GraphQLContext {
   prisma: PrismaClient;
   user: User | null;
 }
+
+export const resolvers = {
+  Query: {
+    viewer() {
+      return { id: 1, name: "John Smith", status: "cached" };
+    },
+  },
+};
+
+export const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    status: String!
+  }
+  type Query {
+    viewer: User
+  }
+`;
+
+export const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
 
 // async function createContext(req: NextApiRequest, res: NextApiResponse) {
 //   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
