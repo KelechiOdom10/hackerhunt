b8a64f9033a9523f7818d159e8ac5801267c88f2
Kelechiodom10 <kelechiodom10@gmail.com>
2022-12-19 14:15:28 +0000

    try this

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,11 +1,11 @@
 import "reflect-metadata";
-import Cors from "micro-cors";
 import { PrismaClient, User } from "@prisma/client";
 import { ApolloServer } from "apollo-server-micro";
 import prisma from "server/db";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import { schema } from "server/schema";
 import { getUser } from "server/utils/auth";
+import NextCors from "nextjs-cors";
 
 export interface GraphQLContext {
   req: NextApiRequest;
@@ -13,6 +13,20 @@ export interface GraphQLContext {
   prisma: PrismaClient;
   user: User | null;
 }
+
+// Next.js API route config
+// https://nextjs.org/docs/api-routes/api-middlewares
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
+
+// Check environment.
+
+// Cache route handler.
+let handler: NextApiHandler;
 
 async function createContext(req: NextApiRequest, res: NextApiResponse) {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -27,36 +41,44 @@ async function createContext(req: NextApiRequest, res: NextApiResponse) {
   };
 }
 
-const apolloServer = new ApolloServer({ schema, context: createContext });
+const apiHandler: NextApiHandler = async (req, res) => {
+  // Run the cors middleware
+  await NextCors(req, res, {
+    origin: true,
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: [
+      "Origin",
+      "Access-Control-Allow-Origin",
+      "Content-Type",
+      "Origin, X-Requested-With, Content-Type, Accept",
+      "X-HTTP-Method-Override, Authorization",
+      "Accept",
+    ],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  });
 
-const startServer = apolloServer.start();
-
-export const config = {
-  api: {
-    bodyParser: false,
-    externalResolver: true,
-  },
-};
-
-const cors = Cors({
-  allowMethods: ["POST", "OPTIONS"],
-  allowHeaders: [
-    "Access-Control-Allow-Origin",
-    "Origin, X-Requested-With, Content-Type, Accept",
-    "X-HTTP-Method-Override, Authorization",
-  ],
-});
-
-const handler = cors(async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "OPTIONS") {
-    return res.status(200).send("ok");
+  // Use cached handler.
+  if (handler) {
+    return handler(req, res);
   }
+
+  // Create Apollo server.
+  const apolloServer = new ApolloServer({
+    schema,
+    context: createContext,
+  });
+
+  const startServer = apolloServer.start();
 
   await startServer;
 
-  await apolloServer.createHandler({
+  // Create new handler.
+  handler = apolloServer.createHandler({
     path: "/api/graphql",
-  })(req, res);
-});
+  });
 
-export default handler;
+  return handler(req, res);
+};
+
+export default apiHandler;
