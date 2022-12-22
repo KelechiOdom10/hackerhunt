d8295e160e46c43435d832221e7c6183181fcff8
Kelechiodom10 <kelechiodom10@gmail.com>
2022-12-22 23:52:12 +0000

    add default meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,22 +4,26 @@ import { ApolloProvider } from "@apollo/client";
 import theme from "../utils/theme";
 import { useApollo } from "../apollo/client";
 import NextNProgress from "nextjs-progressbar";
+import Meta from "~/components/layout/Meta";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState);
   const color = useColorModeValue("#0a0a0a", "#FFFFFF");
 
   return (
-    <ApolloProvider client={client}>
-      <ChakraProvider resetCSS theme={theme}>
-        <NextNProgress
-          color={color}
-          height={4}
-          options={{ showSpinner: false }}
-        />
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </ApolloProvider>
+    <>
+      <Meta />
+      <ApolloProvider client={client}>
+        <ChakraProvider resetCSS theme={theme}>
+          <NextNProgress
+            color={color}
+            height={4}
+            options={{ showSpinner: false }}
+          />
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </ApolloProvider>
+    </>
   );
 }
 
