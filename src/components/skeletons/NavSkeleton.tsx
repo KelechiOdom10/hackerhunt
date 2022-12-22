7f295689c894fc0fa873ca0c4aa1aa7e2e764a24
Kelechiodom10 <kelechiodom10@gmail.com>
2022-12-22 23:49:28 +0000

    add animation to navbar components

--- src/components/skeletons/NavSkeleton.tsx
+++ src/components/skeletons/NavSkeleton.tsx
@@ -1,14 +1,19 @@
-import { Skeleton, Button } from "@chakra-ui/react";
+import { Skeleton, Button, Stack } from "@chakra-ui/react";
 
 export default function NavSkeleton() {
   return (
-    <>
+    <Stack
+      justify="space-around"
+      align="center"
+      direction="row"
+      spacing={{ base: 4, md: 6 }}
+    >
       <Skeleton w="80px" rounded="md">
         <Button />
       </Skeleton>
       <Skeleton w="80px" rounded="md">
         <Button />
       </Skeleton>
-    </>
+    </Stack>
   );
 }
