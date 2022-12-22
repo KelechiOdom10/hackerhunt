1fe7b229641f3aa4683607d4c99ef41e0316c2ba
Kelechiodom10 <kelechiodom10@gmail.com>
2022-12-22 23:48:55 +0000

    add animation to body element

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,8 @@
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex, Box, useColorModeValue } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
+import { motion } from "framer-motion";
 
 interface Props {
   children: ReactNode;
@@ -11,8 +12,21 @@ export default function Layout({ children }: Props) {
   return (
     <Flex flexDir="column" minH="100vh">
       <NavBar />
-      <Box flex="1 0 auto" maxW="6xl" w="100%" mx="auto" py={2} px={4}>
-        {children}
+      <Box bg={useColorModeValue("white", "gray.900")}>
+        <Box
+          as={motion.main}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition="1s ease-in-out .1s"
+          flex="1 0 auto"
+          maxW="6xl"
+          w="100%"
+          mx="auto"
+          py={2}
+          px={4}
+        >
+          {children}
+        </Box>
       </Box>
       <Footer />
     </Flex>
