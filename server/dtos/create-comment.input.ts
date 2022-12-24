2c8ed438268575121cc6c36dab093df050a6048e
Kelechiodom10 <kelechiodom10@gmail.com>
2022-12-24 19:34:12 +0000

    Remove type-graphql decorators

--- server/dtos/create-comment.input.ts
+++ server/dtos/create-comment.input.ts
@@ -1,13 +1,9 @@
-import { Field, InputType } from "type-graphql";
 import { IsNotEmpty } from "class-validator";
 
-@InputType()
 export class CreateCommentInput {
-  @Field(() => String)
   @IsNotEmpty()
   linkId: string;
 
-  @Field(() => String)
   @IsNotEmpty()
   text: string;
 }
--- server/dtos/create-link.input.ts
+++ server/dtos/create-link.input.ts
@@ -1,19 +1,14 @@
-import { Field, InputType } from "type-graphql";
 import { IsArray, IsNotEmpty, IsUrl, Length } from "class-validator";
 
-@InputType()
 export class CreateLinkInput {
-  @Field(() => String)
   @IsNotEmpty()
   @Length(2, 255)
   title: string;
 
-  @Field(() => String)
   @IsNotEmpty()
   @IsUrl()
   url: string;
 
-  @Field(() => [String])
   @IsArray()
   tags: string[];
 }
