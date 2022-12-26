e93c2f98e6cecca5943365d8c2aff6f34f98a5ac
Kelechiodom10 <kelechiodom10@gmail.com>
2022-12-26 16:49:40 +0000

    Add default query options for react query

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,11 +11,24 @@
 //   }
 // }
 
+import { DefaultOptions } from "@tanstack/react-query";
+
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
 export const IS_DEV = !IS_PRODUCTION;
 
 export const API_URL = IS_PRODUCTION
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`
+  ? `http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`
   : "http://localhost:3000/api/graphql";
 
 export const PAGE_SIZE = 10;
+
+export const QUERY_OPTIONS_DEFAULT: DefaultOptions = {
+  queries: {
+    retry: false,
+    staleTime: 15 * 60 * 1000, // 15 minutes
+    enabled: true,
+    refetchInterval: false,
+    refetchOnMount: true,
+    refetchOnWindowFocus: false,
+  },
+};
