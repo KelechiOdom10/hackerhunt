8d49481d3374029e17466a35399f095309f0499b
Kelechiodom10 <kelechiodom10@gmail.com>
2022-12-26 16:46:46 +0000

    Create custom fetcher for codegen

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,7 +1,38 @@
-export const fetcher = (url: string) =>
-  fetch(url).then(res => {
-    if (res.status >= 400) {
-      throw res.statusText;
+import { TOKEN_NAME, parseCookies } from "server/utils/auth-cookies";
+import { API_URL } from "~/config";
+
+export function customFetcher<TData, TVariables>(
+  query: string,
+  variables?: TVariables,
+  headers?: RequestInit["headers"] | undefined
+) {
+  return async (): Promise<TData> => {
+    const token = parseCookies()[TOKEN_NAME];
+    const res = await fetch(API_URL, {
+      method: "POST",
+      ...{
+        headers: {
+          ...headers,
+          authorization: token ? `Bearer ${token}` : "",
+          "Apollo-Require-Preflight": "true",
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      },
+
+      body: JSON.stringify({ query, variables }),
+    });
+
+    const json = await res.json();
+
+    if (json.errors) {
+      console.log({ e: json.errors[0] });
+
+      const { message } = json.errors[0];
+
+      throw new Error(message);
     }
-    return res.json();
-  });
+
+    return json.data;
+  };
+}
