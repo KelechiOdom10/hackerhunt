fb422621704356627b4ff748a3f78984343b641c
Kelechiodom10 <kelechiodom10@gmail.com>
2022-12-26 16:55:52 +0000

    Use secure protocol in prod

--- src/config/index.ts
+++ src/config/index.ts
@@ -17,7 +17,7 @@ export const IS_PRODUCTION = process.env.NODE_ENV === "production";
 export const IS_DEV = !IS_PRODUCTION;
 
 export const API_URL = IS_PRODUCTION
-  ? `http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`
   : "http://localhost:3000/api/graphql";
 
 export const PAGE_SIZE = 10;
